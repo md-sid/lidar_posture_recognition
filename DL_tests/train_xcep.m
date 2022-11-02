72e275e297b84058c7b1fb8fb68c3df93052edff
Siddat <57923667+md-sid@users.noreply.github.com>
2022-11-02 01:27:13 -0600

    train with xception

---
+++ DL_tests/train_xcep.m
@@ -0,0 +1,41 @@
+clear; clc; close all;
+
+rng(0);
+
+imds = imageDatastore('train/', 'IncludeSubfolders', true, 'LabelSource', 'foldernames');
+
+imds.ReadFcn = @customReadDatstoreImage;
+
+[imTrain, imValid, imTest] = splitEachLabel(imds, 0.65, 0.15, 0.20, 'randomized');
+% [imTrain, imValid] = splitEachLabel(imds, 0.8, 0.2, 'randomized');
+
+numClasses = numel(categories(imds.Labels));
+
+%% modify network and set options
+net = xception;
+lgraph = layerGraph(net);
+
+newin = imageInputLayer([16 128 3], 'Name', 'input_fixed');
+lgraph = replaceLayer(lgraph, 'input_1', newin);
+
+newFc = fullyConnectedLayer(numClasses,"Name","new_fc");
+lgraph = replaceLayer(lgraph,"predictions",newFc);
+
+newOut = classificationLayer("Name","new_out");
+lgraph = replaceLayer(lgraph,"ClassificationLayer_predictions",newOut);
+
+options = trainingOptions("sgdm","InitialLearnRate", 0.001, 'ValidationData', imValid, ...
+    'MaxEpochs', 2, 'MiniBatchSize', 32);
+
+%% perform training
+[lid_xcep,info] = trainNetwork(imTrain, lgraph, options);
+
+%% classify test images
+% imTest = imageDatastore('test/', 'IncludeSubfolders', true, 'LabelSource', 'foldernames');
+% imTest.ReadFcn = @customReadDatstoreImage;
+testpreds = classify(lid_xcep,imTest);
+
+%% evaluate result
+nnz(testpreds == imTest.Labels)/numel(testpreds)
+
+confusionchart(imTest.Labels,testpreds);
